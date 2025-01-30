7288451d643a107e2ebc25af694eb450ce7dfff4
Minghao1999 <m_sun11@u.pacific.edu>
2025-01-29 22:51:58 -0800

    finish history orders query function in server side

--- order-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ order-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,9 +1,18 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
+
+    @Select("<script>"
+            + "SELECT * FROM orders WHERE 1=1"
+            + "<if test='status != null'> AND status = #{status} </if>"
+            + "</script>")
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
 }
